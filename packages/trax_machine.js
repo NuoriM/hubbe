9fa33e34d06a2ff3a707cec69af7a4a89b3ada99
NuoriM <34687879+NuoriM@users.noreply.github.com>
2023-12-14 00:55:22 -0300

    correção

--- packages/trax_machine.js
+++ packages/trax_machine.js
@@ -2,10 +2,14 @@ let style = `
 	#draggable-windows-container {position: absolute;top: 0;left: 0;width: 100%;height: 100%;pointer-events: none;overflow: hidden;}
 `;
 let traxStyle = document.querySelector('#trax_style');
-if(!traxStyle) document.head.insertAdjacentHTML('beforeend',`<style>${style}</style`);
+if(!traxStyle) document.head.insertAdjacentHTML('beforeend',`<style>${style}</style>`);
 
 let container = document.querySelector('#draggable-windows-container');
-if(!container) HUBBE.utils.createElement('div',{id:'draggable-windows-container'})
+if(!container) {
+	let draggableWindowsContainer = HUBBE.utils.createElement('div',
+		{id:'draggable-windows-container'});
+	document.body.appendChild(draggableWindowsContainer);
+}
 
 if(!document.querySelector('#trax_machine')){
 	let trax = HUBBE.utils.createElement('div',{id:'trax_machine'});
