0f17aed49afe95314428fa3e3a8065d78a40f163
NuoriM <34687879+NuoriM@users.noreply.github.com>
2023-12-14 01:03:11 -0300

    Inclusão de uma constante onde ficará os elementos DOM

--- packages/trax_machine.js
+++ packages/trax_machine.js
@@ -1,18 +1,25 @@
 let style = `
 	#draggable-windows-container {position: absolute;top: 0;left: 0;width: 100%;height: 100%;pointer-events: none;overflow: hidden;}
 `;
-let traxStyle = document.querySelector('#trax_style');
-if(!traxStyle) document.head.insertAdjacentHTML('beforeend',`<style>${style}</style>`);
+const trax_DOM_elem = {
+	style: null,
+	containers: {
+		draggableWin: null,
+		trax: null,
+	}
+}
+trax_DOM_elem.style = document.querySelector('#trax_style');
+if(!trax_DOM_elem.style) document.head.insertAdjacentHTML('beforeend',`<style>${style}</style>`);
 
-let container = document.querySelector('#draggable-windows-container');
-if(!container) {
-	let draggableWindowsContainer = HUBBE.utils.createElement('div',
+trax_DOM_elem.containers.draggableWin = document.querySelector('#draggable-windows-container');
+if(!trax_DOM_elem.containers.draggableWin) {
+	trax_DOM_elem.containers.draggableWin = HUBBE.utils.createElement('div',
 		{id:'draggable-windows-container'});
-	document.body.appendChild(draggableWindowsContainer);
+	document.body.appendChild(trax_DOM_elem.containers.draggableWin);
 }
 
-if(!document.querySelector('#trax_machine')){
-	let trax = HUBBE.utils.createElement('div',{id:'trax_machine'});
+if(!trax_DOM_elem.containers.trax){
+	trax_DOM_elem.containers.trax = HUBBE.utils.createElement('div',{id:'trax_machine'});
 
-	container.appendChild(trax);
+	trax_DOM_elem.containers.draggableWin.appendChild(trax_DOM_elem.containers.trax);
 }
