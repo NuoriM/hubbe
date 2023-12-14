ca224c8fccd4df49f38550d49144888d9ac93d59
NuoriM <34687879+NuoriM@users.noreply.github.com>
2023-12-14 00:46:25 -0300

    inicio da criação da janela

--- packages/trax_machine.js
+++ packages/trax_machine.js
@@ -1,1 +1,6 @@
-alert('Hello World');
+let container = document.querySelector('#draggable-windows-container');
+if(!document.querySelector('#trax_machine')){
+	let trax = HUBBE.utils.createElement('div',{id:'trax_machine'});
+
+	container.appendChild(trax);
+}
