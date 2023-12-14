cf0ed47c15c4bc7a954d92dc9495924c5fb57e1e
NuoriM <34687879+NuoriM@users.noreply.github.com>
2023-12-14 00:52:53 -0300

    Incluido validações e estilo

--- packages/trax_machine.js
+++ packages/trax_machine.js
@@ -1,4 +1,12 @@
+let style = `
+	#draggable-windows-container {position: absolute;top: 0;left: 0;width: 100%;height: 100%;pointer-events: none;overflow: hidden;}
+`;
+let traxStyle = document.querySelector('#trax_style');
+if(!traxStyle) document.head.insertAdjacentHTML('beforeend',`<style>${style}</style`);
+
 let container = document.querySelector('#draggable-windows-container');
+if(!container) HUBBE.utils.createElement('div',{id:'draggable-windows-container'})
+
 if(!document.querySelector('#trax_machine')){
 	let trax = HUBBE.utils.createElement('div',{id:'trax_machine'});
 
