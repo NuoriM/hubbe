2e832c0a99c6d7de0bc479daeec807040a40db2c
NuoriM <34687879+NuoriM@users.noreply.github.com>
2023-12-16 01:22:50 -0300

    comentado novamente o drag e correção no pointer events

--- packages/trax_machine.js
+++ packages/trax_machine.js
@@ -3,7 +3,7 @@ let images = {
 }
 let style = `
 	#draggable-windows-container {position: absolute;top: 0;left: 0;width: 100%;height: 100%;pointer-events: none;overflow: hidden;}
-	#trax_machine{width: 591px;height: 397px;padding: 31px 16px;background-image:url(${images.traxBg});margin: 0 auto;pointer-events: all;}
+	#trax_machine{width: 591px;height: 397px;padding: 31px 16px;background-image:url(${images.traxBg});margin: 0 auto;pointer-events: none;}
 	.draggable_window{display: inline-block;visibility: hidden;pointer-events: all;}
 `;
 const trax_DOM = {
@@ -74,28 +74,28 @@ function validarTrax(){
 
 // draggable-windows.js
 
-function startDrag(evt) {
+// function startDrag(evt) {
 
-    var diffX = evt.clientX - this.offsetLeft,
-        diffY = evt.clientY - this.offsetTop,
-        that = this;
+//     var diffX = evt.clientX - this.offsetLeft,
+//         diffY = evt.clientY - this.offsetTop,
+//         that = this;
 
-    function moveAlong(evt) {
-        that.style.left = (evt.clientX - diffX) + 'px';
-        that.style.top = (evt.clientY - diffY) + 'px';
-    }
+//     function moveAlong(evt) {
+//         that.style.left = (evt.clientX - diffX) + 'px';
+//         that.style.top = (evt.clientY - diffY) + 'px';
+//     }
 
-    function stopDrag() {
-        document.removeEventListener('mousemove', moveAlong);
-        document.removeEventListener('mouseup', stopDrag);
-    }
+//     function stopDrag() {
+//         document.removeEventListener('mousemove', moveAlong);
+//         document.removeEventListener('mouseup', stopDrag);
+//     }
 
-    document.addEventListener('mouseup', stopDrag);
-    document.addEventListener('mousemove', moveAlong);
-}
+//     document.addEventListener('mouseup', stopDrag);
+//     document.addEventListener('mousemove', moveAlong);
+// }
 
-function startDragIfDraggable(evt) {
-    if (evt.target.classList.contains('draggable-window')) startDrag.call(evt.target, evt);
-}
+// function startDragIfDraggable(evt) {
+//     if (evt.target.classList.contains('draggable-window')) startDrag.call(evt.target, evt);
+// }
 
-document.body.addEventListener('mousedown', startDragIfDraggable);
+// document.body.addEventListener('mousedown', startDragIfDraggable);
